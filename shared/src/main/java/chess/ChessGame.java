1d169b69c3141517989fe935020a946536d24e1c
krleigh <kristenlwhitehouse@gmail.com>
2024-07-08 14:44:57 -0600

    phase 1: moved the test files, began working on the setBoard()

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,6 +10,11 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    public ChessBoard m_board = new ChessBoard();
+    public ChessGame.TeamColor m_teamTurn;
+    private int m_turn;
+
+
     public ChessGame() {
 
     }
@@ -18,7 +23,12 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        if (m_turn % 2 == 0){
+            return TeamColor.BLACK;
+        } else {
+            return TeamColor.BLACK;
+        }
+
     }
 
     /**
@@ -96,7 +106,8 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        m_board.resetBoard();
+
     }
 
     /**
@@ -105,6 +116,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return m_board;
     }
 }
