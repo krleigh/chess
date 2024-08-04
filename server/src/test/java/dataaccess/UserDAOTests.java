cfe143fcdc8ae260b6a0cad56f9c8d6307cbb95d
krleigh <kristenlwhitehouse@gmail.com>
2024-08-04 00:05:24 -0600

    phase 4 : listUsers test isn't quite working, but close

--- server/src/test/java/dataaccess/UserDAOTests.java
+++ server/src/test/java/dataaccess/UserDAOTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import service.requestresult.RegisterRequest;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -35,6 +36,20 @@ public class UserDAOTests {
         var username = userDAO.createUser(request).username();
         assertEquals("lugan", username);
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MySQLUserDAO.class, MemoryUserDAO.class})
+    void listUsersTest(Class<? extends UserDAO> dbClass) throws ResponseException, DataAccessException {
+        UserDAO userDAO = getDataAccess(dbClass);
+
+        ArrayList<UserData> expectedUsers = new ArrayList<>();
+        expectedUsers.add(userDAO.createUser(new RegisterRequest("lugan", "whaleword", "ewhale@gwhale.org")));
+        expectedUsers.add(userDAO.createUser(new RegisterRequest("ella", "secretphant", "peanut@email.com")));
+        expectedUsers.add(userDAO.createUser(new RegisterRequest("cheddar", "thedragon75", "cheese@cmail.com")));
+
+        var users = userDAO.listUsers();
+        assertEquals(expectedUsers, users);
     }
 
 
