2ccb6e10d5911a183aa9f53ff83097eed5299de9
krleigh <kristenlwhitehouse@gmail.com>
2024-08-16 23:40:08 -0600

    phase 5: found that getAdapter is getting stuck when running the register method with client

--- shared/src/main/java/serverfacade/ServerFacade.java
+++ shared/src/main/java/serverfacade/ServerFacade.java
@@ -75,7 +75,7 @@ public class ServerFacade {
 
     private static void writeBody(Object request, HttpURLConnection http) throws IOException {
         if (request != null) {
-            http.addRequestProperty("Content-Type", "application/json");
+            http.setRequestProperty("Content-Type", "application/json");
             String reqData = new Gson().toJson(request);
             try (OutputStream reqBody = http.getOutputStream()) {
                 reqBody.write(reqData.getBytes());
